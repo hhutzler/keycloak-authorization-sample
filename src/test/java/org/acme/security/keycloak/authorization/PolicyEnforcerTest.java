038cbbb7a9939f89b9032b67ca3eafed5710dde3
Helmut Hutzler <helmut.hutzler@th-nuernberg.de>
2022-02-17 16:34:59 +0100

    Adding some Integrations Test

--- src/test/java/org/acme/security/keycloak/authorization/PolicyEnforcerTest.java
+++ src/test/java/org/acme/security/keycloak/authorization/PolicyEnforcerTest.java
@@ -1,10 +1,10 @@
 package org.acme.security.keycloak.authorization;
-
 import org.junit.jupiter.api.Test;
-
 import io.quarkus.test.junit.QuarkusTest;
-import io.quarkus.test.keycloak.client.KeycloakTestClient;
 import io.restassured.RestAssured;
+import org.keycloak.representations.AccessTokenResponse;
+import java.util.HashMap;
+import java.util.Map;
 
 @QuarkusTest
 public class PolicyEnforcerTest {
@@ -12,32 +12,66 @@ public class PolicyEnforcerTest {
         RestAssured.useRelaxedHTTPSValidation();
     }
 
-    KeycloakTestClient keycloakClient = new KeycloakTestClient();
+    private static final String KEYCLOAK_SERVER_URL = System.getProperty("keycloak.url", "http://localhost:8280/auth");
+    private static final String KEYCLOAK_REALM = "RBAC";
+    private static final String CLIENT_ID = "app-client";
+    private static final String CLIENT_SECRET= "0a32b2ad-7b58-4c5b-bffe-7d3673fe70a3";
+
+    // Get an Access Tone
+    private String getAccessToken(String userName, String pwd) {
+        return RestAssured
+                .given()
+                .param("grant_type", "password")
+                .param("username", userName)
+                .param("password", pwd)
+                .param("client_id", CLIENT_ID)
+                .param("client_secret", CLIENT_SECRET)
+                .when()
+                .post(KEYCLOAK_SERVER_URL + "/realms/" + KEYCLOAK_REALM + "/protocol/openid-connect/token")
+                .as(AccessTokenResponse.class).getToken();
+    }
 
     @Test
-    public void testAccessUserResource() {
-        RestAssured.given().auth().oauth2(getAccessToken("alice"))
-                .when().get("/api/users/me")
-                .then()
-                .statusCode(200);
-        RestAssured.given().auth().oauth2(getAccessToken("jdoe"))
-                .when().get("/api/users/me")
+    public void testUserCanViewAccountResource() {
+         RestAssured.given().auth().oauth2(getAccessToken("testuser" ,"xxx"))
+                 .when().get("/accounts")
+                 .then()
+                 .statusCode(200);
+     }
+
+    public void testAdminCanViewAccountResource() {
+        RestAssured.given().auth().oauth2(getAccessToken("testadmin" ,"xxx"))
+                .when().get("/accounts")
                 .then()
                 .statusCode(200);
     }
 
     @Test
-    public void testAccessAdminResource() {
-        RestAssured.given().auth().oauth2(getAccessToken("alice"))
-                .when().get("/api/admin")
+    public void testWrongAccessToken() {
+        RestAssured.given().auth().oauth2("1234")
+                .when().get("/accounts")
+                .then()
+                .statusCode(401);
+    }
+
+    @Test
+    public void testUserCanCreateAccountResource() {
+        Map<String, Object> map = new HashMap<>();
+        map.put("accountname", "Test12");
+        RestAssured.given().auth().oauth2(getAccessToken("testuser" ,"xxx"))
+                .body(map)
+                .when().post("/accounts")
                 .then()
                 .statusCode(403);
-        RestAssured.given().auth().oauth2(getAccessToken("jdoe"))
-                .when().get("/api/admin")
-                .then()
-                .statusCode(403);
-        RestAssured.given().auth().oauth2(getAccessToken("admin"))
-                .when().get("/api/admin")
+    }
+
+    @Test
+    public void testAdminCanCreateAccountResource() {
+        Map<String, Object> map = new HashMap<>();
+        map.put("accountname", "Test12");
+        RestAssured.given().auth().oauth2(getAccessToken("testadmin" ,"xxx"))
+                .body(map)
+                .when().post("/accounts")
                 .then()
                 .statusCode(200);
     }
@@ -48,9 +82,5 @@ public class PolicyEnforcerTest {
                 .when().get("/api/public")
                 .then()
                 .statusCode(204);
-    }
-
-    private String getAccessToken(String userName) {
-        return keycloakClient.getAccessToken(userName);
     }
 }
